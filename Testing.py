ba6c81342794ba23ac23992fffcc3f2a2559c13d
Cristian Campos Agüero <ccampos04@gmail.com>
2026-04-22 08:22:17 -0600

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,41 +1,26 @@
-import Laboratorio5;
+import Laboratorio4;
 import pytest;
     
-def test_eliminarRepetidos_1():
-    assert Laboratorio5.eliminarRepetidos( [12, 78, 12, 0, 5.2, "abc", 0, 12, 5.2, 12] ) == [78, "abc"]
+def test_eliminarPares_1():
+    assert Laboratorio4.eliminarPares( [12, 78, 12, 0, 0, 12, 12] ) == []
     
 def test_eliminarRepetidos_2():
-    assert Laboratorio5.eliminarRepetidos( [12, 5.2, 12] ) == [ 5.2]
+    assert Laboratorio4.eliminarPares( [12, 5, 12] ) == [5]
+
+###############################################################
+def test_sumaValoresPares_1():
+    assert Laboratorio4.sumaValoresPares([0,2,3,4], [4, 8, 6, 0]) == [4,10,0,4]
     
-def test_sumaImparesPares_1():
-    assert Laboratorio5.sumaImparesPares([0,2,3,4], [4, 8, 6, 0]) == [13, 14]
+def test_sumaValoresPares_2():
+    assert Laboratorio4.sumaValoresPares([10, 0], [1,2]) == [0,2]
     
-def test_sumaImparesPares_2():
-    assert Laboratorio5.sumaImparesPares([10, 0], [100, 2]) == [110, 2]
-    
-def test_sumaImparesPares_3():
-    assert isinstance(Laboratorio5.sumaImparesPares([1,2], "dos"), str) == isinstance("Error: segundo argumento debe ser entero", str)
-    
-def test_convertirBase_1():
-    assert Laboratorio5.convertirBase([0,0,1,0] , 2, 10) == 2
+def test_sumaValoresPares_3():
+    assert isinstance(Laboratorio4.sumaValoresPares([1,2], "dos"), str) == isinstance("Error: Los vectores no son del mismo tamaño", str)
+
+######################################################################
+
+def test_convertirBinario_1():
+    assert Laboratorio4.convertirBinario([2,0]) == 10100
 
 def test_convertirBase_2():
-    assert Laboratorio5.convertirBase([2] , 10, 2) == 10
-
-def test_convertirBase_3():
-    assert Laboratorio5.convertirBase(["F","F","F"] , 16, 10) == 4095
-    
-def test_convertirBase_4():
-    assert Laboratorio5.convertirBase([4,0,9,5] , 10, 16) == "FFF"
-    
-def test_convertirBase_5():
-    assert Laboratorio5.convertirBase([7] , 10, 4) == 13
-    
-def test_convertirBase_6():
-    assert Laboratorio5.convertirBase([1,3] , 4, 10) == 7
-    
-def test_convertirBase_7():
-    assert Laboratorio5.convertirBase([2,5,3] , 10, 7) == 511
-
-def test_convertirBase_8():
-    assert Laboratorio5.convertirBase([5,1,1] , 7, 10) == 253 
+    assert Laboratorio4.convertirBinario([8,2,1]) == 1100100001
