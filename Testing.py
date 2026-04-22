ef578be421297a76fa5e685f6786bfe7977cf288
Cristian Campos Agüero <ccampos04@gmail.com>
2026-04-22 08:34:22 -0600

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -7,6 +7,9 @@ def test_eliminarPares_1():
 def test_eliminarRepetidos_2():
     assert Laboratorio4.eliminarPares( [12, 5, 12] ) == [5]
 
+def test_eliminarRepetidos_3():
+    assert isinstance(Laboratorio4.eliminarPares([12, "5", 12] ), str) == isinstance("Error: Los valores deben ser enteros", str)
+
 ###############################################################
 def test_sumaValoresPares_1():
     assert Laboratorio4.sumaValoresPares([0,2,3,4], [4, 8, 6, 0]) == [4,10,0,4]
@@ -24,3 +27,6 @@ def test_convertirBinario_1():
 
 def test_convertirBase_2():
     assert Laboratorio4.convertirBinario([8,2,1]) == 1100100001
+
+def test_convertirBase_3():
+    assert isinstance(Laboratorio4.convertirBinario(20), str) == isinstance("Error: El parámetro debe ser una lista", str)
